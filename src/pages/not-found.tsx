a52d40cf547240c1ac7c3ad28c539c05fe86a644
Michał Worwąg <michalwo@gmail.com>
2025-03-16 13:05:54 +0100

    feat: enhance NotFound page with redirect functionality
    - Add a 3-second redirect to the home page
    - Display message indicating the redirect to users

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
+import { Navigate, useNavigate } from 'react-router';
+
 const NotFound = () => {
+  const [redirect, setRedirect] = useState(false);
+  // const navigate = useNavigate();
+
+  setTimeout(() => {
+    setRedirect(true);
+  }, 3000);
+
+  // useEffect(() => {
+  //   if (redirect) {
+  //     navigate('/');
+  //   }
+  // }, [redirect, navigate]);
+
   return (
     <>
       <h1>404 Not Found</h1>
+      <p>You will be redirected to the home page in 3 seconds.</p>
+      {redirect && <Navigate replace to='/' />}
     </>
   );
 };
